da15a17d242a1affefa587b90893a761109ec3d2
Anton Miller <anton71194@gmail.com>
2020-11-01 15:38:37 -0600

    changed name of the LastGitHub class

--- src/test/java/LastGitHubClass_AntonM.java
+++ src/test/java/LastGitHubClass_AntonM.java
@@ -1,4 +1,4 @@
-public class LastGitHubClass {
+public class LastGitHubClass_AntonM {
     public static void main(String[] args) {
         System.out.println("I have learned a lot about Git and GitHub");
     }
